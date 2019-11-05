7985de608d0d8a99ebd8266e528310b907cf2644
sachinp-work <51262193+sachinp-work@users.noreply.github.com>
2019-11-05 11:24:41 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,116 @@
+import React from 'react';
+import { Text, View, TouchableOpacity, FlatList,ScrollView } from 'react-native';
+import * as ImageManipulator from 'expo-image-manipulator';
+import * as Permissions from 'expo-permissions';
+import { Camera } from 'expo-camera';
+
+const Clarifai = require('clarifai');
+
+const clarifai = new Clarifai.App({
+  apiKey: 'd803f8da7d5a42c6af874d191a225a60',
+});
+process.nextTick = setImmediate;
+
+export default class App extends React.Component {
+  state = {
+    hasCameraPermission: null,
+    predictions: [],
+  };
+  async componentWillMount() {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    this.setState({ hasCameraPermission: status === 'granted' });
+  }
+  capturePhoto = async () => {
+    if (this.camera) {
+      let photo = await this.camera.takePictureAsync();
+      return photo.uri;
+    }
+  };
+  resize = async photo => {
+    let manipulatedImage = await ImageManipulator.manipulateAsync(
+      photo,
+      [{ resize: { height: 300, width: 300 } }],
+      { base64: true }
+    );
+    return manipulatedImage.base64;
+  };
+  predict = async image => {
+    let predictions = await clarifai.models.predict(
+      Clarifai.GENERAL_MODEL,
+      image
+    );
+    
+    return predictions;
+  };
+  objectDetection = async () => {
+    let photo = await this.capturePhoto();
+    let resized = await this.resize(photo);
+    let predictions = await this.predict(resized);
+    
+    //console.log(predictions)
+    this.setState({ predictions: predictions.outputs[0].data.concepts });
+  };
+
+  render() {
+    const { hasCameraPermission, predictions } = this.state;
+    if (hasCameraPermission === null) {
+      return <View />;
+    } else if (hasCameraPermission === false) {
+      return <Text>No access to camera</Text>;
+    } else {
+      return (
+        <View style={{ flex: 1 }}>
+          <Camera
+            ref={ref => {
+              this.camera = ref;
+            }}
+            style={{ flex: 1 }}
+            type={this.state.type}
+          >
+            <View
+              style={{
+                flex: 1,
+                backgroundColor: 'transparent',
+                flexDirection: 'column',
+                justifyContent: 'flex-end'
+              }}
+            >
+              <View
+                style={{
+                  flex: 1,
+                  alignSelf: 'flex-start',
+                  alignItems: 'center',
+                }}
+              >
+                <FlatList
+                  style={{marginTop:50}}
+                  data={ 
+                    predictions.map(prediction => ({
+                    key: `${prediction.name} `
+                  }))}
+                  renderItem={({ item }) => (
+                    <Text style={{  color: 'white', fontSize: 20 }}>{item.key}</Text>
+                  )}
+                />
+              </View>
+              <TouchableOpacity
+                style={{
+                  flex: 0.1,
+                  alignItems: 'center',
+                  backgroundColor: 'blue',
+                  height: '10%',
+                }}
+                onPress={this.objectDetection}
+              >
+                <Text style={{ fontSize: 30, color: 'white', padding: 15 }}>
+                  {' '}
+                  Detect Objects{' '}
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </Camera>
+        </View>
+      );
+    }
+  }
+}
